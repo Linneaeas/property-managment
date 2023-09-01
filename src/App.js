57f35ffa19f31ce67f3b44c484671d1eb8228420
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-09-01 15:40:31 +0200

    Export/Import StartPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { StartPage } from "./Views/start-page.js";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <StartPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Views/start-page.js
@@ -0,0 +1,11 @@
+export function StartPage() {
+  return (
+    <div className="StartPage">
+      <h1>Please choose user:</h1>
+      <button>Admin</button>
+      <button>Front Desk</button>
+      <button>Housekeeping</button>
+      <button>Maintainence</button>
+    </div>
+  );
+}
