98fa75aa2c427479cbb13115e8ae541a864ce5ed
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-09-05 14:50:34 +0200

    It did, back to before I started with routing :)

--- src/Views/start-page.js
+++ src/Views/start-page.js
@@ -5,7 +5,6 @@ export function StartPage() {
         <h1>CHOOSE USER</h1>
       </header>
       <div className="StartPage">
-        <button className="StartPageBTN"> Checking if Braches worked </button>
         <button className="StartPageBTN"> ADMIN </button>
         <button className="StartPageBTN"> FRONT DESK </button>
         <button className="StartPageBTN"> HOUSEKEEPING </button>
