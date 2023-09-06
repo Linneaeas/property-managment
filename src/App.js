ea943ea34ad5a76ab9f194c215d7d03a7abb13d8
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-09-06 19:22:32 +0200

    Tried routing w links, adm dont show, but switches btwn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
 import { StartPage } from "./Views/start-page.js";
+import { AdminStart } from "./Views/admin-start";
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <StartPage />
-      </div>
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/Views/admin-start"> Admin</Link>
+        </nav>
+
+        <Routes>
+          <Route index element={<StartPage />} />
+          <Route path="Admin" element={<AdminStart />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/Views/admin-start.js
+++ src/Views/admin-start.js
@@ -1,11 +1,11 @@
-export function StartPage() {
+export function AdminStart() {
   return (
     <div className="StartPageWrap">
       <header className="Header" id="StartHeader">
         <h1>CHOOSE USER</h1>
       </header>
       <nav className="StartPage">
-        <button className="StartPageBTN"> ADMIN </button>
+        <button className="StartPageBTN"> Schould be at admin </button>
         <button className="StartPageBTN"> FRONT DESK </button>
         <button className="StartPageBTN"> HOUSEKEEPING </button>
         <button className="StartPageBTN"> MAINTAINENCE </button>
