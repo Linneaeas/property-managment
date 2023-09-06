fcca3ae8d1cfd533564d8b8c3be5d39393454594
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-09-06 21:34:01 +0200

    Link->BTN OK! Rout in rout prob later?SP hidden IF not SP?  add backBTN on ASP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { useState } from "react";
-import { StartPage } from "./Views/start-page.js";
 import { AdminStart } from "./Views/admin-start";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/Admin"> Admin</Link>
-        </nav>
+      <div className="StartPageWrap">
+        <header className="Header" id="StartHeader">
+          <h1>CHOOSE USER</h1>
+        </header>
+        <BrowserRouter>
+          <nav className="StartPage">
+            <Link to="/Admin">
+              {" "}
+              <button type="button" className="StartPageBTN">
+                ADMIN
+              </button>
+            </Link>
+          </nav>
 
-        <Routes>
-          <Route index element={<StartPage />} />
-          <Route path="Admin" element={<AdminStart />} />
-        </Routes>
-      </BrowserRouter>
+          <Routes>
+            <Route path="Admin" element={<AdminStart />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
--- src/Views/admin-start.js
+++ src/Views/admin-start.js
@@ -1,15 +1,7 @@
 export function AdminStart() {
   return (
-    <div className="StartPageWrap">
-      <header className="Header" id="StartHeader">
-        <h1>CHOOSE USER</h1>
-      </header>
-      <nav className="StartPage">
-        <button className="StartPageBTN"> Schould be at admin </button>
-        <button className="StartPageBTN"> FRONT DESK </button>
-        <button className="StartPageBTN"> HOUSEKEEPING </button>
-        <button className="StartPageBTN"> MAINTAINENCE </button>
-      </nav>
+    <div className="AdminStartPageWrap">
+      <nav className="AdminStartPage"> At Admin</nav>
     </div>
   );
 }
