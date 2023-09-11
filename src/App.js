e988cbab44b3e8bb2f000487342f6c0cd3be8efb
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-09-11 16:55:32 +0200

    Added Loginform w Admin user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import "./App.css";
-import React from "react";
-import { Route, Routes, Link } from "react-router-dom";
-import { Home } from "./Views/home";
-import { AdminStart } from "./Views/admin-start";
+import React, { useState } from "react";
+import LoginForm from "./Components/LoginForm";
 
 function App() {
-  return <div className="App"></div>;
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+
+    if (
+      details.email == adminUser.email &&
+      details.password == adminUser.password
+    ) {
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      console.log("Details don't match");
+      setError("Details don't match");
+    }
+  };
+
+  const Logout = () => {
+    setUser({ name: "", email: "" });
+  };
+
+  return (
+    <main className="App">
+      {user.email != "" ? (
+        <div classname="welcome">
+          <h2>
+            Welcome, <span>{user.name}</span>
+          </h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
+    </main>
+  );
 }
 
 export default App;
---
+++ src/Views/Admin/admin-start.js
@@ -0,0 +1,7 @@
+export function AdminStart() {
+  return (
+    <div>
+      <h2>Admin Start</h2>
+    </div>
+  );
+}
