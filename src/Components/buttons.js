0600e9edafc0ebff9a416a6ae7813cd862eda6dc
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-09-18 17:41:04 +0200

    Standards BTNS OK. Next time store save to? + CancelBtn+press anywhere means Cancel+Add this to when creating new item.

---
+++ src/Components/buttons.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+export default function LogoutButton({ Logout }) {
+  return (
+    <div>
+      <button className="LogoutBTN" id="LogoutButton" onClick={Logout}>
+        LOG OUT
+      </button>
+    </div>
+  );
+}
+
+export function AddButton({ onAdd }) {
+  return (
+    <div>
+      <button className="AddBTN" id="AddButton" onClick={onAdd}></button>
+    </div>
+  );
+}
+
+export function EditButton({ onEdit }) {
+  return (
+    <div>
+      <button className="EditBTN" id="EditButton" onClick={onEdit}></button>
+    </div>
+  );
+}
+
+export function SaveButton({ onSave }) {
+  return (
+    <div>
+      <button className="SaveBTN" id="SaveButton" onClick={onSave}></button>
+    </div>
+  );
+}
+
+export function DeleteButton({ onDelete }) {
+  return (
+    <div>
+      <button
+        className="DeleteBTN"
+        id="DeleteButton"
+        onClick={onDelete}
+      ></button>
+    </div>
+  );
+}
+
+export function CancelButton({ onCancel }) {
+  return (
+    <div>
+      <button
+        className="CancelBTN"
+        id="CancelButton"
+        onClick={onCancel}
+      ></button>
+    </div>
+  );
+}
--- src/Views/Admin/property-standards.js
+++ src/Views/Admin/property-standards.js
@@ -1,10 +1,27 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
+import {
+  AddButton,
+  EditButton,
+  SaveButton,
+  DeleteButton,
+  CancelButton,
+} from "../../Components/buttons";
 
-const DataTable = ({ standards }) => {
-  const handleEdit = (id) => {
-    // Implement your edit logic here, using the item's ID
-    console.log("Editing item with ID:", id);
+export function DataTable({
+  standards,
+  onEdit,
+  onDelete,
+  onSave,
+  setStandards,
+}) {
+  const handleCancel = (id) => {
+    const updatedStandards = standards.map((item) => {
+      if (item.id === id) {
+        return { ...item, isEditing: false, editedName: item.standardName };
+      }
+      return item;
+    });
+    setStandards(updatedStandards);
   };
 
   return (
@@ -12,23 +29,46 @@ const DataTable = ({ standards }) => {
       <tbody>
         {standards.map((item) => (
           <tr key={item.id}>
-            <td id="EditBTNBox">
-              <button
-                className="EditBTN"
-                onClick={() => handleEdit(item.id)}
-              ></button>
+            <td id="editBTNBox">
+              <EditButton onEdit={() => onEdit(item.id)} />
             </td>
-            <td id="StandardNameBox">{item.standardName}</td>
+            <td id="standardNameBox">
+              {item.isEditing ? (
+                <input
+                  type="text"
+                  value={item.editedName}
+                  onChange={(e) => {
+                    const updatedStandards = standards.map((standard) => {
+                      if (standard.id === item.id) {
+                        return { ...standard, editedName: e.target.value };
+                      }
+                      return standard;
+                    });
+                    setStandards(updatedStandards);
+                  }}
+                />
+              ) : (
+                item.standardName
+              )}
+            </td>
+            <td id="saveOrDeleteBTNBox">
+              {item.isEditing ? (
+                <React.Fragment>
+                  <CancelButton onCancel={() => handleCancel(item.id)} />
+                  <DeleteButton onDelete={() => onDelete(item.id)} />
+                  <SaveButton onSave={() => onSave(item.id)} />
+                </React.Fragment>
+              ) : null}
+            </td>
           </tr>
         ))}
       </tbody>
     </table>
   );
-};
+}
 
-const AdminPropertyStandards = () => {
+export function AdminPropertyStandards() {
   const [standards, setStandards] = useState([]);
-
   const [showInput, setShowInput] = useState(false);
   const [newStandardName, setNewStandardName] = useState("");
 
@@ -41,6 +81,8 @@ const AdminPropertyStandards = () => {
       const newStandard = {
         id: standards.length + 1,
         standardName: newStandardName,
+        isEditing: false,
+        editedName: "",
       };
       setStandards([...standards, newStandard]);
       setNewStandardName("");
@@ -48,32 +90,65 @@ const AdminPropertyStandards = () => {
     }
   };
 
+  const handleEdit = (id) => {
+    const updatedStandards = standards.map((item) => {
+      if (item.id === id) {
+        return { ...item, isEditing: true, editedName: item.standardName };
+      }
+      return { ...item, isEditing: false };
+    });
+    setStandards(updatedStandards);
+  };
+
+  const handleCancel = (id) => {
+    const updatedStandards = standards.map((item) => {
+      if (item.id === id) {
+        return { ...item, isEditing: false, editedName: item.standardName };
+      }
+      return item;
+    });
+    setStandards(updatedStandards);
+  };
+
+  const handleSave = (id) => {
+    const updatedStandards = standards.map((item) => {
+      if (item.id === id) {
+        return { ...item, isEditing: false, standardName: item.editedName };
+      }
+      return item;
+    });
+    setStandards(updatedStandards);
+  };
+
+  const handleDelete = (id) => {
+    const updatedStandards = standards.filter((item) => item.id !== id);
+    setStandards(updatedStandards);
+  };
+
   return (
     <div className="PropertyContainer">
       <div className="PropertyContent">
         <h1>PROPERTY STANDARDS</h1>
-        <DataTable standards={standards} />
-        {!showInput && (
-          <button className="AddBTN" onClick={handleAddButtonClick}>
-            Add
-          </button>
-        )}
+        <DataTable
+          standards={standards}
+          onEdit={handleEdit}
+          onSave={handleSave}
+          onDelete={handleDelete}
+          setStandards={setStandards}
+        />
+        {!showInput && <AddButton onAdd={handleAddButtonClick} />}
         {showInput && (
-          <div>
+          <div className="AddContent">
             <input
               type="text"
               value={newStandardName}
               onChange={(e) => setNewStandardName(e.target.value)}
               placeholder="Enter standard name"
             />
-            <button className="SaveBTN" onClick={handleAddStandard}>
-              Save
-            </button>
+            <SaveButton onSave={handleAddStandard} />
           </div>
         )}
       </div>
     </div>
   );
-};
-
-export default AdminPropertyStandards;
+}
