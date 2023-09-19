38ac51c5b85b8b1a7456041542c0f6ce2fe456c9
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-09-19 12:27:44 +0200

    added explanations. Letting them be this long until i 100% understand everything.

--- src/Views/Admin/property-standards.js
+++ src/Views/Admin/property-standards.js
@@ -6,7 +6,7 @@ import {
   DeleteButton,
 } from "../../Components/buttons";
 import OutsideClickListener from "../../Components/event-listeners";
-
+// DataTable component renders a table with rows based on standards data:
 export function DataTable({
   standards,
   onEdit,
@@ -20,18 +20,23 @@ export function DataTable({
   return (
     <table className="PropertyTable">
       <tbody>
+        {/*{standards.map((item) => ( is using the map function on the standards array. It's iterating over each item in the standards array and creating elements for each:*/}
         {standards.map((item) => (
           <tr key={item.id}>
+            {/*This is creating a table row (<tr>) for each item in the standards array. The key is used to uniquely identify each row.*/}
             <td id="editBTNBox">
+              {/*This is creating a table cell (<td>) with the ID "editBTNBox".*/}
               <EditButton onEdit={() => onEdit(item.id)} />
+              {/*This is rendering an EditButton component and passing it a function to call when clicked, which calls the onEdit function with the item.id.*/}
             </td>
             <td id="standardNameBox">
-              {" "}
-              {/* Remove whitespace here */}
+              {/*This is creating a table cell (<td>) with the ID "standardNameBox".*/}
+              {/*{item.isEditing ? (...) : (...)}: This is a conditional rendering based on the isEditing property of the item. If isEditing is true, it renders an input field; otherwise, it renders the standard name.*/}
               {item.isEditing ? (
                 <input
                   type="text"
                   value={item.editedName}
+                  /*onChange={(e) => {...}}: This is an event handler for when the input value changes. It's using an arrow function to define the behavior.Inside the function, it's creating a new array updatedStandards using map. It's iterating over each standard in the standards array.For the standard that matches the item.id, it's creating a new object using spread syntax ({ ...standard, editedName: e.target.value }). It copies all properties of the standard and updates the editedName to the new value from the input.For other standards, it keeps them unchanged.Finally, it calls setStandards to update the standards state with the new array of standards.onClick={(e) => e.stopPropagation()}: This is an event handler for the click event. It prevents the click event from propagating further up the DOM tree, stopping any parent elements from also receiving the click event.When the input value changes (when you type something in the input field), the onChange event handler updates the editedName for the respective standard in the standards array and then updates the state with the updated standards using setStandards. The value of the input is controlled by the editedName property of the item.*/
                   onChange={(e) => {
                     const updatedStandards = standards.map((standard) =>
                       standard.id === item.id
@@ -47,12 +52,13 @@ export function DataTable({
               )}
             </td>
             <td id="saveOrDeleteBTNBox">
-              {" "}
-              {/* Remove whitespace here */}
+              {/* Render delete and save button when editing: */}
               {item.isEditing && (
                 <>
                   <DeleteButton onDelete={() => onDelete(item.id)} />
                   <SaveButton onSave={() => onSave(item.id)} />
+                  {/*<Rendering a DeleteButton component and passing it a function to call when clicked, which calls the onDelete function with the item.id.
+                  Rendering a SaveButton component and passing it a function to call when clicked, which calls the onSave function with the item.id.*/}
                 </>
               )}
             </td>
@@ -63,19 +69,22 @@ export function DataTable({
   );
 }
 
+// AdminPropertyStandards component for managing property standards
 export function AdminPropertyStandards() {
-  const [standards, setStandards] = useState([]);
-  const [showInput, setShowInput] = useState(false);
-  const [newStandardName, setNewStandardName] = useState("");
+  const [standards, setStandards] = useState([]); // State for property standards
+  const [showInput, setShowInput] = useState(false); // State for input visibility
+  const [newStandardName, setNewStandardName] = useState(""); // State for new standard name
   const [isAddingNewStandard, setIsAddingNewStandard] = useState(false); // Define isAddingNewStandard
   const [isEditingStandard, setIsEditingStandard] = useState(false); // Define isEditingStandard
 
+  // Handler for add button click:
   const handleAddButtonClick = () => {
     setNewStandardName(""); // Reset new standard name when clicking add
     setShowInput(true);
     setIsAddingNewStandard(true); // Set isAddingNewStandard to true when starting to add
   };
 
+  // Handler for adding a new standard:
   const handleAddStandard = () => {
     if (newStandardName.trim() !== "") {
       const newStandard = {
@@ -91,6 +100,7 @@ export function AdminPropertyStandards() {
     }
   };
 
+  // Handler for editing a standard
   const handleEdit = (id) => {
     const updatedStandards = standards.map((item) => {
       if (item.id === id) {
@@ -106,6 +116,7 @@ export function AdminPropertyStandards() {
     setStandards(updatedStandards);
   };
 
+  // Handler for saving changes to a standard:
   const handleSave = (id) => {
     const updatedStandards = standards.map((item) => {
       if (item.id === id) {
@@ -117,11 +128,14 @@ export function AdminPropertyStandards() {
     setStandards(updatedStandards);
   };
 
+  // Handler for deleting a standard:
   const handleDelete = (id) => {
     const updatedStandards = standards.filter((item) => item.id !== id);
     setStandards(updatedStandards);
   };
 
+  // Handler for outside click to handle editing and adding states:
+  //Creating a new Const since a lot or errors kept occouring so needed to specify the function. In Return: <OutsideClickListener onOutsideClick={handleOutsideClick}>, is telling the OutsideClickListener component, "Hey, when an outside click happens, don't use your default behavior; instead, use this handleOutsideClick function."
   const handleOutsideClick = () => {
     if (isAddingNewStandard && !isEditingStandard) {
       setIsAddingNewStandard(false);
