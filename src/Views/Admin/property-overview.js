3f5011d00d42278fef14fe011e9dcb9ef22d2391
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-09-19 13:50:16 +0200

    Displaying information on Overview

--- src/Views/Admin/property-overview.js
+++ src/Views/Admin/property-overview.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AdminStart } from "./admin-start";
+import { getStandardsFromLocalStorage } from "../../Components/local-storage";
+
 export function AdminPropertyOverview() {
+  const [standards, setStandards] = useState([]);
+
+  useEffect(() => {
+    // Load standards from local storage when the component mounts
+    const savedStandards = getStandardsFromLocalStorage();
+    if (savedStandards) {
+      setStandards(savedStandards);
+    }
+  }, []); // Empty dependency array ensures this effect runs once on component mount
+
   return (
     <div className="PropertyContainer">
       <div className="PropertyContent">
         <h1>Property Overview</h1>
+        <h2>Property standards:</h2>
+        <ul>
+          {standards.map((standard) => (
+            <li key={standard.id}>{standard.standardName}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
