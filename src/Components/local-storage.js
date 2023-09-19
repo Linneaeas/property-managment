5cdd802b7796ef30f6ce70624ba8a5b120fc3767
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-09-19 14:58:25 +0200

    Added suites to local storage

--- src/Components/local-storage.js
+++ src/Components/local-storage.js
@@ -1,4 +1,4 @@
-// Function to save standards to local storage
+// STANDARDS
 export const saveStandardsToLocalStorage = (standards) => {
   try {
     const serializedStandards = JSON.stringify(standards);
@@ -7,17 +7,37 @@ export const saveStandardsToLocalStorage = (standards) => {
     console.error("Error saving standards to local storage:", error);
   }
 };
-
-// Function to get standards from local storage
 export const getStandardsFromLocalStorage = () => {
   try {
     const serializedStandards = localStorage.getItem("standards");
     if (serializedStandards === null) {
-      return undefined; // Standards not found in local storage
+      return undefined;
     }
     return JSON.parse(serializedStandards);
   } catch (error) {
     console.error("Error getting standards from local storage:", error);
+    return undefined;
+  }
+};
+
+//SUITES
+export const saveSuitesToLocalStorage = (suites) => {
+  try {
+    const serializedSuites = JSON.stringify(suites);
+    localStorage.setItem("suites", serializedSuites);
+  } catch (error) {
+    console.error("Error saving suites to local storage:", error);
+  }
+};
+export const getSuitesFromLocalStorage = () => {
+  try {
+    const serializedSuites = localStorage.getItem("suites");
+    if (serializedSuites === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedSuites);
+  } catch (error) {
+    console.error("Error getting suites from local storage:", error);
     return undefined;
   }
 };
--- src/Views/Admin/property-suites.js
+++ src/Views/Admin/property-suites.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { getStandardsFromLocalStorage } from "../../Components/local-storage";
+import {
+  getStandardsFromLocalStorage,
+  saveSuitesToLocalStorage,
+  getSuitesFromLocalStorage,
+} from "../../Components/local-storage";
 
 export function AdminPropertySuites() {
   const [standards, setStandards] = useState([]);
@@ -13,13 +17,13 @@ export function AdminPropertySuites() {
     <div className="PropertyContainer">
       <div className="PropertyContent">
         <h1>SUITES</h1>
-        <input list="standardOptions" />
-        <datalist id="standardOptions">
-          {standards.map((standard) => (
-            <option key={standard.id} value={standard.standardName} />
-          ))}
-        </datalist>
       </div>
     </div>
   );
 }
+
+/*<input list="standardOptions" /><datalist id="standardOptions">
+{standards.map((standard) => (
+  <option key={standard.id} value={standard.standardName} />
+))}
+</datalist> */
